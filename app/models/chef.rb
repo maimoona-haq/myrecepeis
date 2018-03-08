faf19c210805c424b16aafb9217004a79d34b31a
maimoona-haq <maimoonahaq84@gmail.com>
2018-03-08 15:14:26 +0500

    Implemented a signup page

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,3 +1,5 @@
 class  Chef < ApplicationRecord
 has_many :recipes
+has_secure_password
+validates :password, presence: true, length: {minimum: 5}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302154805) do
+ActiveRecord::Schema.define(version: 20180308040744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180302154805) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "recipes", force: :cascade do |t|
