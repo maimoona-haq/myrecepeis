cc929630cd452242386a1ac8a622dcbfd3e80848
maimoona-haq <maimoonahaq84@gmail.com>
2018-03-09 06:34:56 +0500

    add pagination

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,5 +1,10 @@
 class ChefsController < ApplicationController
 
+    def index
+    @chefs = Chef.paginate(page: params[:page], per_page: 5) 
+    end
+
+
     def new
         @chef = Chef.new
     end
